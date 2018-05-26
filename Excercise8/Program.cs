20019c720c58cf645af231a1a42ef39ac3e6b6d5
kkwaguchi <keikawaguchikawaguchi@gmail.com>
2018-05-26 18:38:56 +0900

    課題８

--- Excercise8/Program.cs
+++ Excercise8/Program.cs
@@ -6,13 +6,25 @@ namespace Excercise8
     {
         static void Main(string[] args)
         {
-            Console.Write("数値を入力してください：");
+            Console.Write("正の整数を入力してください：");
             var inputText = Console.ReadLine();
             int inputNumber = 0;
-            if (int.TryParse(inputText, out inputNumber)){
-                Console.WriteLine("入力した値は整数です");
+            bool isPrimeNumber = true;
+            if (int.TryParse(inputText, out inputNumber)　&& inputNumber > 0){
+                for(int i = 2;i < inputNumber/2 + 1;i++){
+                    if(inputNumber%i == 0){
+                        isPrimeNumber = false;
+                        break;
+                    }
+                }
+                if(isPrimeNumber){
+                    Console.WriteLine("入力した値は素数です");
+                }else{
+                    Console.WriteLine("入力した値は素数ではありません");    
+                }
+
             }else{
-                Console.WriteLine("入力した値は整数ではありません");
+                Console.WriteLine("入力した値は正の整数ではありません");
             }
         }
     }
