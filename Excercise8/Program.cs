f70b9d1b607743b7e49fb0377cf5e7ae4acf4251
kkwaguchi <keikawaguchikawaguchi@gmail.com>
2018-05-26 18:00:02 +0900

    課題８作成

---
+++ Excercise8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数値を入力してください：");
+            var inputText = Console.ReadLine();
+            int inputNumber = 0;
+            if (int.TryParse(inputText, out inputNumber)){
+                Console.WriteLine("入力した値は整数です");
+            }else{
+                Console.WriteLine("入力した値は整数ではありません");
+            }
+        }
+    }
+}
