f066062a9f4c286ccef0df402afab1aa23801b89
kkwaguchi <keikawaguchikawaguchi@gmail.com>
2018-05-26 17:50:54 +0900

    課題６作成

---
+++ Excercise6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Excercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1;i<+100;i++){
+                if(i%5 == 0){
+                    Console.WriteLine("Buzz");
+                }else{
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
