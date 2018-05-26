40a0d694150330bb84c5c017345abddaae13225b
kkwaguchi <keikawaguchikawaguchi@gmail.com>
2018-05-26 15:05:54 +0900

    課題３作成

---
+++ Excercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Excercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力してください：");
+            var inputText = Console.ReadLine();
+            Console.WriteLine("入力値：" + inputText);
+        }
+    }
+}
