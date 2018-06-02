c15253f1a381e0e25eda8134ea6114d2e7d02121
kkwaguchi <keikawaguchikawaguchi@gmail.com>
2018-06-02 13:56:21 +0900

    課題９追加

---
+++ Excercise9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
