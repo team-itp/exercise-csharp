875902fe503eca3315bde3576bed2ce2615f3ddf
Yuto Nagano <chamelion@hotmail.co.jp>
2019-01-26 11:01:07 +0900

    カレンダーの課題3を作成

---
+++ 102/Calendar3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calendar3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderCalendar(0, 28);
+            RenderCalendar(0, 30);
+            RenderCalendar(0, 31);
+
+            RenderCalendar(1, 30);
+        }
+
+        private static void RenderCalendar(int startDayOfWeek, int lengthOfMonth)
+        {
+            Console.WriteLine("日 月 火 水 木 金 土");
+
+            for (var i = 0; i < startDayOfWeek; i++)
+            {
+                Console.Write("   ", i);
+            }
+            for (var i = 1; i <= lengthOfMonth; i++)
+            {
+                Console.Write("{0, 2}", i);
+                if ((i + startDayOfWeek) % 7 == 0 || i == lengthOfMonth)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ", i);
+                }
+            }
+        }
+    }
+}
