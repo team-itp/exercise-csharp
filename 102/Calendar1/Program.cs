cfc6ad0a689adae31d0379633d54c3f50f14fa18
Yuto Nagano <chamelion@hotmail.co.jp>
2019-01-26 10:48:33 +0900

    カレンダー課題1を作成

---
+++ 102/Calendar1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calendar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("日 月 火 水 木 金 土");
+            for (var i = 1; i <= 31; i++)
+            {
+                Console.Write("{0, 2}", i);
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ", i);
+                }
+            }
+        }
+    }
+}
