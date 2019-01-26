71e93f3136e3c509dfb295603889a4b7f98eec8c
Yuto Nagano <chamelion@hotmail.co.jp>
2019-01-26 10:56:21 +0900

    課題2のサンプルコードを作成

---
+++ 102/Calendar2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calendar2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderCalendar(0);
+            RenderCalendar(1);
+            RenderCalendar(2);
+            RenderCalendar(3);
+            RenderCalendar(4);
+            RenderCalendar(5);
+            RenderCalendar(6);
+
+            // startDayOfWeek を 7 以上に設定すると表示が崩れる
+            RenderCalendar(7);
+        }
+
+        private static void RenderCalendar(int startDayOfWeek)
+        {
+            Console.WriteLine("日 月 火 水 木 金 土");
+
+            for (var i = 0; i < startDayOfWeek; i++)
+            {
+                Console.Write("   ", i);
+            }
+            for (var i = 1; i <= 31; i++)
+            {
+                Console.Write("{0, 2}", i);
+                if ((i + startDayOfWeek) % 7 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ", i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
