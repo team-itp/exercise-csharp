1f9e2184815b45ac33f1427d32a284acaeb25a35
Yuto Nagano <chamelion@hotmail.co.jp>
2019-02-02 15:43:29 +0900

    WinForms でプロジェクトを新規作成

---
+++ 103/Timesheet.WinForms/DailyInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Timesheet.WinForms
+{
+    public partial class DailyInput : Form
+    {
+        public DailyInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 103/Timesheet.WinForms/MonthlyReport.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Timesheet.WinForms
+{
+    public partial class MonthlyReport : Form
+    {
+        public MonthlyReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
